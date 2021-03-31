a9691fef146d1a39d1507b945dd872523be96ecc
Wirya Ramadhan <adhaniscuber@gmail.com>
2021-03-31 23:44:18 +0700

    day 2

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Wirya" + "Ramadhan")
+
+	fmt.Printf("1 + 1 =", 1+1)
+	fmt.Printf("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
